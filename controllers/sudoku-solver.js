f9ea20b50e6db60a3d8e3f6ac50828aa7af0642b
Lok <lok@pg95.com>
2024-06-05 14:51:18 +0100

    finished solver and unit test

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,143 @@
 class SudokuSolver {
+  
+  constructor (){
+    this.solution = ''
+  }
 
   validate(puzzleString) {
+    if (puzzleString.length != 81) return false;
+    if (puzzleString.search(/[^0-9.]/) >= 0 ) return false;
+    return true;
+  }
+
+  getRow(puzzleString, row){
+    let rowStringArr=[]
+    let start = 9*(row-1);
+    for (let i = start; i < start+9; i++){
+      rowStringArr.push(puzzleString[i])
+    }
+    //console.log(rowStringArr);
+    return rowStringArr
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    let rowStringArr = this.getRow(puzzleString, row);
+    //console.log('row:' + row + ' col: ' + column + ' value: '+ value)
+    if (rowStringArr[column-1] != '.' || rowStringArr.indexOf(value.toString()) >= 0 ) {
+      //console.log('row failed');
+      return false;
+    }
+    return true
+  }
 
+  getCol(puzzleString, column){
+    let colStringArr=[];
+
+    for(let i=column-1; i<81; i+=9){
+      colStringArr.push(puzzleString[i]);
+    }
+    //console.log(colStringArr);
+    return colStringArr;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    let colStringArr=this.getCol(puzzleString, column);
 
+    if (colStringArr[row-1] != '.' || colStringArr.indexOf(value.toString()) > 0 ) {
+      //console.log('col failed');
+      return false;
+    }
+    return true;
+  }
+
+  getRegion(puzzleString, zone){
+    //zone 1 - 9     1,4,7
+    let row, col
+    let blkStringArr = [];
+
+    row = [1,4,7][Math.floor((zone-0.1)/3)]
+    col = [1,4,7][Math.floor((zone-0.1)%3)] 
+    for (let i=0; i<3; i++) {
+      for(let j=0; j<3; j++) { 
+        blkStringArr.push(puzzleString[(row-1+i) * 9 + col-1+j])
+      }
+    }
+    //console.log(blkStringArr)
+    return blkStringArr;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    let rowStart = Math.floor((row-1)/3)*3 + 1    // start row of the 3x3 square
+    let colStart = Math.floor((column-1)/3)*3 + 1 // start column of the 3x3 square
+    let pos = (row - rowStart)*3 + (column-colStart) + 1 
+    let zone = (rowStart-1)+(colStart-1)/3 + 1
+    let blkStringArr = this.getRegion(puzzleString, zone);
+    //console.log('row: '+ rowStart + ' col: '+ colStart + ' pos: ' + pos + ' zone: ' + zone)
 
+    if (blkStringArr[pos-1] != '.' || blkStringArr.indexOf(value.toString()) > 0 ) {
+      //console.log('region failed');
+      return false;
+    }
+    return true;
+  }
+
+  checkPlacement(puzzleString, row, column, value) {
+    if (this.checkRowPlacement(puzzleString, row, column, value) &&
+        this.checkColPlacement(puzzleString, row, column, value) &&
+        this.checkRegionPlacement(puzzleString, row, column, value)) {
+      return true;  
+    }
+  }
+
+  check(puzzleString, finished) {
+    if (this.validate(puzzleString))
+    if (finished) {
+      if (!this.validate(puzzleString) || puzzleString.indexOf('.') >=0)
+      return false;
+    }
+      
+    for (let i=1; i<=9; i++) {
+      if (this.haveDuplicate(this.getRow(puzzleString, i))) return false;
+      if (this.haveDuplicate(this.getCol(puzzleString, i))) return false;
+      if (this.haveDuplicate(this.getRegion(puzzleString, i))) return false;
+    }
+    return true;
+  }
+
+  haveDuplicate(arr) {
+   // console.log(arr)
+   let duplicate = false
+    arr.map((item, index) => {
+      //console.log(index + '|' + arr.indexOf(item))
+      if (item !== '.' && arr.indexOf(item) !== index) {
+        duplicate = true
+      }
+    })
+    return duplicate;
   }
 
   solve(puzzleString) {
-    
+    let puzzleArr = puzzleString.split('')
+
+    let nextEmpty = puzzleString.indexOf('.')
+    let row = Math.floor(nextEmpty/9)+1
+    let col = Math.floor(nextEmpty)%9+1
+
+    if (nextEmpty >= 0) {
+      for (let guess=1; guess<10 ; guess++) {
+        //console.log('row: ' + row + ' col: ' + col + ' guess: ' + guess )
+        if (this.checkPlacement(puzzleString, row, col, guess)) {
+            puzzleArr[nextEmpty]=guess;
+            if (this.solve(puzzleArr.join(''))) return true;
+        } else {
+          puzzleArr[nextEmpty] = '.';
+        }
+      }
+      return false;  // cannot found valid guess
+    } else {  // all filled up
+      this.solution = puzzleArr.join('')
+      return true;
+    }
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,96 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const puzzleArr = require('../controllers/puzzle-strings.js').puzzlesAndSolutions;
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const SudokuSolver = require('../controllers/sudoku-solver.js');
+
+let solver = new Solver;
 
 suite('Unit Tests', () => {
+    test ('#1 handles a valid puzzle string of 81 character', function (done){
+        assert.equal(solver.validate('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'),true);
+        done();
+    })
 
+    test ('#2 handles a puzzle string with invalid characters', function (done){
+        assert.equal(solver.validate('X.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'),false)
+        done();
+    })
+
+    test ('#3 handles a puzzle string that is not 81 characters in length', function (done){
+        assert.equal(solver.validate('91.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'),false);
+        done();
+    })
+
+    test ('#4 Check handles a valid row placement', function (done){
+        assert.equal(solver.checkRowPlacement(puzzleArr[0][0],1,2,9), true);
+        assert.equal(solver.checkRowPlacement(puzzleArr[0][0],3,3,9), true);
+        done();
+    })
+
+    test ('#5 Check handles a invalid row placement', function (done){
+        assert.equal(solver.checkRowPlacement(puzzleArr[0][0],1,3,9), false);
+        assert.equal(solver.checkRowPlacement(puzzleArr[0][0],1,4,8), false);
+        done();
+    })
+
+    test ('#6 Check handles a valid column placement', function (done){
+        assert.equal(solver.checkColPlacement(puzzleArr[0][0],1,2,4), true);
+        assert.equal(solver.checkColPlacement(puzzleArr[0][0],3,3,4), true);
+        done();
+    })
+
+    test ('#7 Check handles an invalid column placement', function (done){
+        assert.equal(solver.checkColPlacement(puzzleArr[0][0],3,2,4), false);
+        assert.equal(solver.checkColPlacement(puzzleArr[0][0],3,3,9), false);
+        done();
+    })
+
+    test ('#8 Check handles an valid region placement', function (done){
+        assert.equal(solver.checkRegionPlacement(puzzleArr[0][0],7,5,5), true);
+        assert.equal(solver.checkRegionPlacement(puzzleArr[0][0],9,9,3), false);
+        done();
+    })
+
+    test ('#9 Check handles an invalid region placement', function (done){
+        assert.equal(solver.checkRegionPlacement(puzzleArr[0][0],3,2,4), false);
+        assert.equal(solver.checkRegionPlacement(puzzleArr[0][0],4,7,4), false);
+        done();
+    })
+
+    test ('#10 Valid puzzle strings pass the solver', function (done){
+        assert.equal(solver.check(puzzleArr[0][0]), true);
+        assert.equal(solver.check(puzzleArr[0][1], true), true);
+        done();
+    })
+
+    test ('#11 Invalid puzzle strings fail the solver', function (done){
+        assert.equal(solver.check(puzzleArr[0][0], true), false);
+        assert.equal(solver.check('227549163531672894649831527496157382218396475753284916962415738185763249374928651'), false);
+        done();
+    })
+
+    test ('#12 Solver returns the expected solution for an incompleted puzzle', function (done){
+        let solver = new SudokuSolver();
+        solver.solve(puzzleArr[0][0])
+        assert.equal(solver.solution, puzzleArr[0][1])
+        solver.solve(puzzleArr[1][0])
+        assert.equal(solver.solution, puzzleArr[1][1])
+        done();
+    })
 });
+
+        //console.log('puzzleArr:')
+        //console.log(puzzleArr[0][0]);
+        // 1.5   ..2   .84
+        // ..6   3.1   2.7
+        // .2.   .5.   ...
+        
+        // .9.   .1.   ...
+        // 8.2   .36   74.
+        // 3.7   .2.   .9.
+        
+        // 47.   ..8   ..1
+        // ..1   6..   ..9
+        // 269   14.   37.
